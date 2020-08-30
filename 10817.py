0e79da3632114910707c077558d940bd137e1075
Firoms <edisonmessi@naver.com>
2020-08-30 15:11:54 +0900

    2020/08/30

---
+++ 10817.py
@@ -0,0 +1,7 @@
+a, b, c = map(int, input().split(" "))
+li = []
+li.append(a)
+li.append(b)
+li.append(c)
+li.sort()
+print(li[1])
---
+++ 11720.py
@@ -0,0 +1,6 @@
+T = int(input())
+A = input()
+b = 0
+for i in range(T):
+    b += int(A[i])
+print(b)
