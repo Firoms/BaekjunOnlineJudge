9213823b64a526704446cbf57390adcf3bd9b32f
Firoms <edisonmessi@naver.com>
2020-08-31 17:16:11 +0900

    2020/08/31

---
+++ 1546.py
@@ -0,0 +1,11 @@
+c = int(input())
+li = (input().split(" "))
+for i in range(len(li)):
+    li[i] = int(li[i])
+li.sort()
+for i in range(len(li)):
+    li[i] = int(li[i])/int(li[-1])*100
+a = 0
+for i in li:
+    a += float(i)
+print(a/c)
