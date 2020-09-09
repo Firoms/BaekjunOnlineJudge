14ca349eb92b216dadc55b22cefb896f6fac129b
Firoms <edisonmessi@naver.com>
2020-09-09 21:33:36 +0900

    2020/09/09

---
+++ practice.py
@@ -0,0 +1,24 @@
+
+# import turtle as t
+# side = 100
+# angle = 90
+# for j in range(4):
+#     for i in range(4):
+#         t.fd(side)
+#         t.right(angle)
+#     t.left(angle)
+
+# import math
+# a = int(input("반지름을 입력하세요:"))
+# b = "%0.4f" % float(a*a*math.pi)
+# print(f"반지름이 {a}인 원의 넓이 = {b}")
+
+
+# import turtle as t
+# radius = 50
+# for i in range(3):
+#     t.pendown()
+#     t.circle(radius)
+#     radius += 20
+#     t.penup()
+#     t.fd(100)
