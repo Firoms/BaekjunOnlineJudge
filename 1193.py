2d9e0b436972c4f8cab3f046a4f82127673ac32d
Firoms <edisonmessi@naver.com>
2020-09-17 20:02:26 +0900

    2020/09/17

---
+++ 1193.py
@@ -0,0 +1,16 @@
+a = int(input())
+a += 1
+time = 0
+b = 1
+i = 0
+while a > b:
+    b += i
+    i += 1
+    time += 1
+
+b -= i-1
+c = a-b
+if time % 2 == 1:
+    print(f"{c}/{time-c}")
+else:
+    print(f"{time-c}/{c}")
