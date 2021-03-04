131dd4a471aef9efc1ff8d65abe7ef3a7eb333d5
Firoms <edisonmessi@naver.com>
2021-03-04 21:47:27 +0900

    2021/03/04

---
+++ 1010.py
@@ -0,0 +1,10 @@
+# 다리 놓기
+import sys
+T = int(sys.stdin.readline().rstrip())
+for _ in range(T):
+    M, N = map(int, sys.stdin.readline().split())
+    cnt = 1
+    for n in range(M):
+        cnt *= N-n
+        cnt /= n+1
+    print(int(cnt))
