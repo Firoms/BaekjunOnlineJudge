ef9387300dacdb644648abcb8e7ad643a56318b5
Firoms <edisonmessi@naver.com>
2021-03-20 21:57:37 +0900

    2021/03/20

--- 1267.py
+++ 1267.py
@@ -1,13 +1,14 @@
 # 핸드폰 요금
 import sys
+
 N = int(sys.stdin.readline().rstrip())
 call_li = list(map(int, sys.stdin.readline().split()))
 Y, M = 0, 0
 for i in call_li:
-    Y += (i//30 + 1)*10
-    M += (i//60 + 1)*15
-if Y<=M:
+    Y += (i // 30 + 1) * 10
+    M += (i // 60 + 1) * 15
+if Y <= M:
     print("Y", end=" ")
-if Y>=M:
+if Y >= M:
     print("M", end=" ")
 print(min([Y, M]))
