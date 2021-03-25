40604a712f438905e658a8592b144840330e8e51
Firoms <edisonmessi@naver.com>
2021-03-25 21:01:20 +0900

    2021/03/25

---
+++ 1284.py
@@ -0,0 +1,15 @@
+# 집 주소
+import sys
+while True:
+    num = sys.stdin.readline().rstrip()
+    if num=='0':
+        break
+    size = 1
+    for i in num:
+        if i=='0':
+            size += 5
+        elif i=='1':
+            size += 3
+        else:
+            size += 4
+    print(size)
