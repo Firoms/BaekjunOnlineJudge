1f230054cebab0fe39e094a19652bcbfb0249e7a
Firoms <edisonmessi@naver.com>
2021-08-14 21:47:14 +0900

    2021/08/14

--- Level2/1026.py
+++ Level2/1026.py
@@ -1,5 +1,6 @@
 # 보물
 import sys
+
 N = int(sys.stdin.readline().rstrip())
 A = list(map(int, sys.stdin.readline().split()))
 B = list(map(int, sys.stdin.readline().split()))
@@ -7,5 +8,5 @@ A.sort()
 B.sort(reverse=True)
 S = 0
 for i in range(N):
-    S += A[i]*B[i]
+    S += A[i] * B[i]
 print(S)
--- Level2/1037.py
+++ Level2/1037.py
@@ -1,9 +1,10 @@
 # 약수
 import sys
+
 amount = sys.stdin.readline().rstrip()
 divisors = list(map(int, sys.stdin.readline().split()))
 
-if len(divisors)==1:
-    print(divisors[0]**2)
+if len(divisors) == 1:
+    print(divisors[0] ** 2)
 else:
-    print(max(divisors)*min(divisors))
+    print(max(divisors) * min(divisors))
