d2579f69289453b215c3f102c768eb8c32223beb
Firoms <edisonmessi@naver.com>
2021-08-19 19:02:34 +0900

    2021/08/19

--- Level3/1016.py
+++ Level3/1016.py
@@ -3,9 +3,10 @@
 
 
 import sys
+
 minNum, maxNum = map(int, sys.stdin.readline().split())
 noNum = 0
-for i in range(minNum, maxNum +1):
-    if i**0.5 != int(i**0.5):
+for i in range(minNum, maxNum + 1):
+    if i ** 0.5 != int(i ** 0.5):
         noNum += 1
 print(noNum)
