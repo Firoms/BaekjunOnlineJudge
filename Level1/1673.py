786b43867847dc5f5d52cf77afea3eb716ff7d00
Firoms <edisonmessi@naver.com>
2022-01-29 12:13:57 +0900

    2022/01/29

---
+++ Level1/1673.py
@@ -0,0 +1,18 @@
+# 치킨 쿠폰
+import sys
+
+while True:
+    try:
+        k, n = map(int, sys.stdin.readline().split())
+        chicken = 0
+        while True:
+            a, b = divmod(k, n)
+            if a==0:
+                chicken += b
+                break
+            else:
+                chicken += a*n
+                k = a+b
+        print(chicken)
+    except:
+        break
