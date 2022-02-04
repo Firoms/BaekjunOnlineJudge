35dc66270d22147cd67dd6150247f5fe837e8422
Firoms <edisonmessi@naver.com>
2022-02-04 18:51:27 +0900

    2022/02/04

--- Level1/1592.py
+++ Level1/1592.py
@@ -1,5 +1,6 @@
 # 영식이와 친구들
 import sys
+
 N, M, L = map(int, sys.stdin.readline().split())
 
 friends = [0 for i in range(N)]
@@ -8,23 +9,23 @@ time = 0
 friends[ball] += 1
 
 while True:
-    if M==1:
+    if M == 1:
         break
     time += 1
-    if friends[ball]%2==0:
+    if friends[ball] % 2 == 0:
         ball -= L
         if ball < 0:
             ball += N
-        friends[ball]+=1
-        if friends[ball]==M:
+        friends[ball] += 1
+        if friends[ball] == M:
             break
-    
+
     else:
         ball += L
         if ball >= N:
             ball -= N
-        friends[ball]+=1
-        if friends[ball]==M:
+        friends[ball] += 1
+        if friends[ball] == M:
             break
 
 print(time)
