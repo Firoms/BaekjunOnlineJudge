73a2f3f39a938eba55803ea3534897551acaeb5a
Firoms <edisonmessi@naver.com>
2022-02-10 20:19:49 +0900

    2022/02/10

--- Level1/1418.py
+++ Level1/1418.py
@@ -1,20 +1,21 @@
 # K-세준수
 
 import sys
+
 N = int(sys.stdin.readline().rstrip())
 K = int(sys.stdin.readline().rstrip())
 
 nums = [0 for _ in range(N)]
 for i in range(N):
-    if i==0:
+    if i == 0:
         continue
-    if nums[i]==0:
-        for j in range(i, N, i+1):
-            nums[j]=i+1
+    if nums[i] == 0:
+        for j in range(i, N, i + 1):
+            nums[j] = i + 1
 
 cnt = 0
 for i in nums:
-    if i<=K:
+    if i <= K:
         cnt += 1
 
 print(cnt)
--- Level1/1524.py
+++ Level1/1524.py
@@ -4,13 +4,13 @@ import sys
 
 T = int(sys.stdin.readline().rstrip())
 for i in range(T):
-    
+
     blank = sys.stdin.readline().rstrip()
     N, M = map(int, sys.stdin.readline().split())
     N_list = map(int, sys.stdin.readline().split())
     M_list = map(int, sys.stdin.readline().split())
 
-    if max(N_list)>=max(M_list):
+    if max(N_list) >= max(M_list):
         print("S")
     else:
         print("B")
