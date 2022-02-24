44a3cd3be2ec37b0364d99cd01c0417d24674db7
Firoms <edisonmessi@naver.com>
2022-02-24 22:59:38 +0900

    2022/02/24

--- Level2/1312.py
+++ Level2/1312.py
@@ -1,18 +1,1 @@
 # 소수 (실패)
-
-import sys
-
-A, B, N = map(int, sys.stdin.readline().split())
-
-result = str(A / B) + "0"
-alp = []
-for i in range(2, len(result)):
-    if result[i] in alp:
-        break
-    else:
-        alp.append(result[i])
-
-if "0" in alp and N > len(alp):
-    print(0)
-else:
-    print(alp[(N - 1) % (len(alp))])
