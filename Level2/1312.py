f782dc9d3a0e51fef8f7449ff5b27eaa1533c7da
Firoms <edisonmessi@naver.com>
2022-02-26 20:58:59 +0900

    2022/02/26

--- Level2/1312.py
+++ Level2/1312.py
@@ -1,1 +1,14 @@
-# 소수 (실패)
+# 소수
+
+import sys
+A, B, N = map(int, sys.stdin.readline().split())
+
+A = A%B
+cnt = 0
+
+while cnt<N:
+    cnt += 1
+    A *= 10
+    result = A//B
+    A = A%B
+print(result)
