dbd9c34d2e9c6f8a6a966f32971d8375d7d2b720
Firoms <edisonmessi@naver.com>
2022-03-26 23:18:55 +0900

    2022/03/26

--- Level1/16428.py
+++
@@ -1,7 +0,0 @@
-# A/B - 3
-import sys
-A, B = map(int, sys.stdin.readline().split())
-
-share, left = divmod(A, B)
-print(share)
-print(left)
--- Level3/2038.py
+++ Level3/2038.py
@@ -9,7 +9,12 @@ def golomb_recursion(pre, cur):
         return 0
     
     sum_pre = sum([i for i in range(pre[0], pre[1]+1)])
+    len_cur = cur[1]-cur[0]+1
+    len_pre = pre[1]-pre[0]+1
 
+    len_new_cur = sum_pre*len_cur/len_pre
+
+    golomb_recursion(cur, new_cur)
 
 
 golomb_recursion([2, 2], [3, 3])
