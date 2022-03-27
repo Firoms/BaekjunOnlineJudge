213cf2f4528654570964bfe772a84467eed78eb6
Firoms <edisonmessi@naver.com>
2022-03-27 21:12:22 +0900

    2022/03/27

--- Level3/2038.py
+++ Level3/2038.py
@@ -7,12 +7,12 @@ n = int(sys.stdin.readline().rstrip())
 def golomb_recursion(pre, cur):
     if cur[0] <= n <= cur[1]:
         return 0
-    
-    sum_pre = sum([i for i in range(pre[0], pre[1]+1)])
-    len_cur = cur[1]-cur[0]+1
-    len_pre = pre[1]-pre[0]+1
 
-    len_new_cur = sum_pre*len_cur/len_pre
+    sum_pre = sum([i for i in range(pre[0], pre[1] + 1)])
+    len_cur = cur[1] - cur[0] + 1
+    len_pre = pre[1] - pre[0] + 1
+
+    len_new_cur = sum_pre * len_cur / len_pre
 
     golomb_recursion(cur, new_cur)
 
