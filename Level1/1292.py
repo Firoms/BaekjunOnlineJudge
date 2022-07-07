275078c9c70979ce5b7c217c6106b3f19f121035
firoms <edisonmessi@naver.com>
2022-07-07 18:09:10 +0900

    2022/07/07

---
+++ Level1/1292.py
@@ -0,0 +1,17 @@
+# 쉽게 푸는 문제
+import sys
+A, B = map(int, sys.stdin.readline().split())
+
+num = 1
+num_sum = 0
+
+check = 1
+
+while check <= B:
+    for i in range(num):
+        if A <= check <= B:
+            num_sum += num
+        check += 1
+    num += 1
+
+print(num_sum)
