c24df8ce12fcc4dacf485c952dfee9bb916829de
firoms <edisonmessi@naver.com>
2022-07-25 21:20:38 +0900

    2022/07/25

---
+++ Level1/1731.py
@@ -0,0 +1,13 @@
+# 추론
+import sys
+T = int(sys.stdin.readline().rstrip())
+
+num_list = []
+
+for _ in range(T):
+    num_list.append(int(sys.stdin.readline().rstrip()))
+
+if num_list[1]-num_list[0]==num_list[2]-num_list[1]:
+    print(num_list[-1]+(num_list[1]-num_list[0]))
+else:
+    print(int(num_list[-1]*(num_list[1]/num_list[0])))
