6487f960c214e4e3950bdeed64a93229ddc8d458
firoms <edisonmessi@naver.com>
2022-08-03 19:05:48 +0900

    2022/08/03

---
+++ Level1/1773.py
@@ -0,0 +1,9 @@
+# 폭죽쇼
+import sys
+N, C = map(int, sys.stdin.readline().split())
+time = [0]*(C+1)
+for _ in range(N):
+    i = int(sys.stdin.readline().rstrip())
+    for j in range(i,C+1,i):
+        time[j] = 1
+print(sum(time))
