7069a44cf758eceb73b7e1eafed8851a4138a32b
firoms <edisonmessi@naver.com>
2022-08-10 19:31:21 +0900

    2022/08/10

--- Level1/1813.py
+++ Level1/1813.py
@@ -4,7 +4,7 @@ import sys
 
 num_dict = {}
 for i in range(51):
-    num_dict[i]=0
+    num_dict[i] = 0
 
 N = int(sys.stdin.readline().rstrip())
 nums = list(map(int, sys.stdin.readline().split()))
@@ -14,8 +14,8 @@ for i in nums:
 
 check = False
 for i in range(51):
-    if num_dict[50-i] == 50-i:
-        print(50-i)
+    if num_dict[50 - i] == 50 - i:
+        print(50 - i)
         check = True
         break
 if not check:
--- Level2/1063.py
+++ Level2/1063.py
@@ -2,4 +2,3 @@
 import sys
 
 K, R, T = map(int, sys.stdin.readline().split())
-
