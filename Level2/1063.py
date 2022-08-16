5e34464400a815dbf239f323bb691b7113f241e5
firoms <edisonmessi@naver.com>
2022-08-16 19:30:39 +0900

    2022/08/16

--- Level2/1063.py
+++ Level2/1063.py
@@ -3,38 +3,39 @@ import sys
 
 K, R, T = sys.stdin.readline().split()
 
+
 def move(O, lr, ud):
     alp = O[0]
     num = int(O[1])
-    alp_to_num = {'A':1, 'B':2, 'C':3, 'D':4, 'E':5, 'F':6, 'G':7, 'H':8}
-    num_to_alp = {1:'A', 2:'B', 3:'C', 4:'D', 5:'E', 6:'F', 7:'G', 8:'H'}
-    
-    new_alp = num_to_alp[max([min([alp_to_num[alp]+lr, 8]), 1])]
-    new_num = str(max([min([num+ud, 8]), 1]))
+    alp_to_num = {"A": 1, "B": 2, "C": 3, "D": 4, "E": 5, "F": 6, "G": 7, "H": 8}
+    num_to_alp = {1: "A", 2: "B", 3: "C", 4: "D", 5: "E", 6: "F", 7: "G", 8: "H"}
+
+    new_alp = num_to_alp[max([min([alp_to_num[alp] + lr, 8]), 1])]
+    new_num = str(max([min([num + ud, 8]), 1]))
     return new_alp + new_num
 
 
 for _ in range(int(T)):
     M = sys.stdin.readline().rstrip()
-    
-    if M=='R':
+
+    if M == "R":
         K = move(K, 1, 0)
-    elif M=='L':
+    elif M == "L":
         K = move(K, -1, 0)
-    elif M=='B':
+    elif M == "B":
         K = move(K, 0, -1)
-    elif M=='T':
+    elif M == "T":
         K = move(K, 0, 1)
-    elif M=='RT':
+    elif M == "RT":
         K = move(K, 1, 1)
-    elif M=='LT':
+    elif M == "LT":
         K = move(K, -1, 1)
-    elif M=='RB':
+    elif M == "RB":
         K = move(K, 1, -1)
-    elif M=='LB':
+    elif M == "LB":
         K = move(K, -1, -1)
     else:
-        #오류
+        # 오류
         pass
-    
+
 print(K)
